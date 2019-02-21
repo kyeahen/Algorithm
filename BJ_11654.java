13e50b6e1d90bb0482cc05d2e4d7dfef43bab6fb
kyeahen <bright19961@naver.com>
2019-02-21 14:07:14 +0900

    Add 11654

---
+++ BJ_11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BJ_11654 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        System.out.print(Integer.valueOf(input.charAt(0)));
+    }
+}
