50f66991175ebc40a16b3f65b869da15ed43ef0f
kyeahen <bright19961@naver.com>
2019-03-06 13:26:21 +0900

    Add 2941

---
+++ BJ_2941.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BJ_2941 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String[] croArray = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String input = s.next();
+
+        for (int i = 0; i < croArray.length; i++) {
+            input = input.replace(croArray[i], "r");
+        }
+
+        System.out.print(input.length());
+    }
+}
