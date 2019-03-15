fb420bf8e080fce98d4a973979fe409c1a0c9143
kyeahen <bright19961@naver.com>
2019-03-15 20:23:14 +0900

    Add 1676

---
+++ BJ_1676.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BJ_1676 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int num = s.nextInt();
+
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 5 == 0) {
+                count++;
+            }
+
+            if(i % 25 == 0) {
+                count++;
+            }
+
+            if (i % 125 == 0) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+
+}
