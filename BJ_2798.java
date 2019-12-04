d2219ad143d0a4477b979b24e957b251670cc103
kyeahen <bright19961@naver.com>
2019-12-04 14:28:04 +0900

    Add 서울에서 김서방 찾기

---
+++ BJ_2798.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class BJ_2798 {
+}
---
+++ P_12919.java
@@ -0,0 +1,19 @@
+public class P_12919 {
+    static String KIM = "Kim";
+
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+        System.out.print(solution(seoul));
+    }
+
+    public static String solution(String[] seoul) {
+        int x = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals(KIM)) {
+                x = i;
+            }
+        }
+
+        return "김서방은 " + x + "에 있다";
+    }
+}
