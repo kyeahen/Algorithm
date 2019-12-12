01ef9f4ba7dc389d1e995e7cc1c0b106c85109b4
kyeahen <bright19961@naver.com>
2019-12-13 01:48:57 +0900

    Modify K번째 수 - level1

--- P_42748.java
+++ P_42748.java
@@ -8,12 +8,12 @@ public class P_42748 {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
-        System.out.print(Arrays.toString(solution(array, commands)));
+        System.out.print(Arrays.toString(solution1(array, commands)));
     }
 
-    public static int[] solution(int[] array, int[][] commands) {
+    public static int[] solution1(int[] array, int[][] commands) {
         ArrayList<Integer> tempArr = new ArrayList<>();
-        ArrayList<Integer> resultArr = new ArrayList<>();
+        int[] resultArr = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
             tempArr.clear();
@@ -26,9 +26,25 @@ public class P_42748 {
             }
 
             Collections.sort(tempArr);
-            resultArr.add(tempArr.get(k));
+            resultArr[i] = resultArr[k];
         }
 
-        return resultArr.stream().mapToInt(i -> i).toArray();
+        return resultArr;
+    }
+
+    public static int[] solution2(int[] array, int[][] commands) {
+        int[] resultArr = new int[commands.length];
+
+        for (int i = 0 ; i < commands.length; i++) {
+            int a = commands[i][0] - 1;
+            int b = commands[i][1] - 1;
+            int k = commands[i][2] - 1;
+
+            int[] temp = Arrays.copyOfRange(array, a, b);
+            Arrays.sort(temp);
+            resultArr[i] = temp[k];
+        }
+
+        return resultArr;
     }
 }
