24e9c02975f118501d2726f63c57703fa216f429
kyeahen <bright19961@naver.com>
2019-12-12 18:36:25 +0900

    Add 나누어 떨어지는 숫자 배열 - level1

---
+++ P_12910.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//나누어 떨어지는 숫자 배열 - level1
+public class P_12910 {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 36, 1, 3};
+        int divisor = 1;
+
+        System.out.print(Arrays.toString(solution(arr, divisor)));
+    }
+
+    public static int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> resultArr = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                resultArr.add(arr[i]);
+            }
+        }
+
+        if (resultArr.size() == 0) {
+            resultArr.add(-1);
+        }
+
+        Collections.sort(resultArr);
+
+        return resultArr.stream().mapToInt(i -> i).toArray();
+    }
+}
