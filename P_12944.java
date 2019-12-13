56dcb5c79dfb7c5c01127b8aef9105327b678ce4
kyeahen <bright19961@naver.com>
2019-12-13 17:26:07 +0900

    Add 평균 구하기 - level1

---
+++ P_12944.java
@@ -0,0 +1,17 @@
+//평균 구하기 - level1
+public class P_12944 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.print(solution(arr));
+    }
+
+    public static double solution(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+
+        double average = (double) sum / arr.length;
+        return average;
+    }
+}
