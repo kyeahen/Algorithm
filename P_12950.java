d590614283ba451ba54513b357a43fe997b3c188
kyeahen <bright19961@naver.com>
2019-12-13 21:44:03 +0900

    Add 행렬의 덧셈 - level1

---
+++ P_12950.java
@@ -0,0 +1,18 @@
+//행렬의 덧셈 - level1
+public class P_12950 {
+    public static void main(String[] args) {
+        int[][] arr1 = {{1, 2}, {2, 3}};
+        int[][] arr2 = {{3, 4}, {5, 6}};
+    }
+
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] resultArr = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j< arr1[0].length; j++) {
+                resultArr[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return resultArr;
+    }
+}
