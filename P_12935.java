5f6d7a614d77055630fba6037179ccdb54ba5c85
kyeahen <bright19961@naver.com>
2019-12-13 15:59:46 +0900

    Add 제일 작은 수 제거하기 - level1

---
+++ P_12935.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//제일 작은 수 제거하기 - level1
+public class P_12935 {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 1};
+        System.out.print(Arrays.toString(solution(arr)));
+    }
+
+    public static int[] solution(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != min) {
+                resultArr.add(arr[i]);
+            }
+        }
+
+        if (resultArr.size() == 0) {
+            resultArr.add(-1);
+        }
+
+        return resultArr.stream().mapToInt(i -> i).toArray();
+    }
+}
