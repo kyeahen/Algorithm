486b2815ddb18cfff989b0cc8e3ebe454734ad46
kyeahen <bright19961@naver.com>
2019-12-13 14:37:24 +0900

    Add 정수 내림차순으로 배치하기 - level1

---
+++ P_12933.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+//정수 내림차순으로 배치하기 - level1
+public class P_12933 {
+    public static void main(String[] args) {
+        int n = 118372;
+        System.out.print(solution(n));
+    }
+
+    public static long solution(long n) {
+        String num = Long.toString(n);
+        char[] numArr = num.toCharArray();
+
+        Arrays.sort(numArr);
+
+        String result = "";
+        for (int i = numArr.length - 1; i >= 0; i--) {
+            result += numArr[i];
+
+        }
+
+        return Long.parseLong(result);
+    }
+}
