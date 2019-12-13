7997b9969aa8ff2d757b042275e0a4e347c29812
kyeahen <bright19961@naver.com>
2019-12-13 21:58:38 +0900

    Add 직사각형 별찍기 - level1

---
+++ P_12969.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//직사각형 별찍기 - level1
+public class P_12969 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
