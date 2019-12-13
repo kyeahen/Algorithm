744dd02f76d6129fb988d316452219af3625a431
kyeahen <bright19961@naver.com>
2019-12-13 13:41:59 +0900

    Add 자릿수 더하기 - level1

---
+++ P_12931.java
@@ -0,0 +1,17 @@
+//자릿수 더하기 - level1
+public class P_12931 {
+    public static void main(String[] args) {
+        int n = 987;
+        System.out.print(solution(n));
+    }
+
+    public static int solution(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+}
