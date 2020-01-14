62c84d8a05631d634d7b590446bb9cdad37d08e7
kyeahen <bright19961@naver.com>
2020-01-14 14:17:37 +0900

    Add 콜라츠 추측, 완주하지 못한 선수 - level1

---
+++ P_12943.java
@@ -0,0 +1,27 @@
+//콜라츠 추측 - level1
+public class P_12943 {
+    public static void main(String[] args) {
+        long num = 626331;
+        System.out.print(solution(num));
+    }
+
+    public static int solution(long num) {
+
+        int i = 0;
+        while(num != 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num * 3 + 1;
+            }
+            i++;
+
+            if (i == 500) {
+                i = -1;
+                break;
+            }
+        }
+
+        return i;
+    }
+}
