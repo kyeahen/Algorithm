624d5ca74dbc105ff3da13c2d75f4e1ed9f8b06d
kyeahen <bright19961@naver.com>
2020-01-17 11:27:00 +0900

    Add 탑 - level2

---
+++ P_42588.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+//탑 - level2
+public class P_42588 {
+
+    public static void main(String[] args) {
+        int[] heights = {6, 9, 5, 7, 4};
+        System.out.print(Arrays.toString(solution(heights)));
+    }
+
+    public static int[] solution(int[] heights) {
+
+        int[] arr = new int[heights.length];
+
+        int start = heights.length - 1;
+
+        for (int i = start; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (heights[i] < heights[j]) {
+                    arr[i] = j + 1;
+                    break;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
