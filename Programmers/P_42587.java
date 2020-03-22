267089bea0d25d97679a2d825382e95d60ce5e77
kyeahen <bright19961@naver.com>
2020-03-22 22:16:59 +0900

    Add 프린터 - level2

--- Programmers/P_42587.java
+++ Programmers/P_42587.java
@@ -1,58 +1,48 @@
 package Programmers;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-
-class Document {
-    int idx; //문서의 원래 순서 (인덱스)
-    int priority; //문서의 우선 순위
-
-    public Document(int idx, int priority) {
-        this.idx = idx;
-        this.priority = priority;
-    }
-}
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 //프린터 - level2
 public class P_42587 {
 
     public static void main(String[] args) {
         int[] priorities = {2, 1, 3 ,2};
-        int location = 0;
+        int location = 2;
+
         System.out.print(solution(priorities, location));
     }
 
-    public static int solution (int[] priorities, int location) {
+    public static int solution(int[] priorities, int location) {
 
-        LinkedList<Document> list = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
 
+         /*
+        우선순위가 높은 문서대로 우선순위 큐에 저장된다. (내림차순)
+        ex) [3, 2, 2, 1]
+        */
         for (int i = 0; i < priorities.length; i++) {
-            list.add(new Document(i, priorities[i]));
+            pq.offer(priorities[i]);
         }
 
-        int count = 1;
-        while (count < priorities.length) {
+        boolean flag = false; // 내가 인쇄 요청한 문서인지를 판별할 상태 변수
+        int count = 0; // 문서 인쇄 순서를 카운팅하는 변수
 
-            int max = 0;
-            int maxIdx = 0;
-            for (int i = 0; i < list.size(); i++) {
+        while (flag != true) { // 내가 인쇄 요청한 문서를 찾을 때까지 반복
 
-                if (max < list.get(i).priority) {
-                    max = list.get(i).priority;
-                    priorities[list.get(i).idx] = count;
-                    maxIdx = i;
+            for (int i = 0; i < priorities.length; i++) {
+
+                if (pq.peek() == priorities[i]) { // 내가 인쇄 요청한 문서를 찾았을 때, 해당 문서의 인덱스를 알기 위해 배열과 비교
+                    pq.poll(); // 큐에서 삭제 (인쇄)
+                    count++; // 문서 인쇄 순서 증가
+
+                    if (i == location) { // 내가 인쇄 요청한 문서일 때
+                        flag = true;
+                        break; // 반복문 탈출
+                    }
                 }
-                list.addLast(list.get(i));
             }
-
-            list.remove(maxIdx);
-            count++;
-            System.out.println(list.size());
         }
 
-//        System.out.print(Arrays.toString(priorities));
-
-        return priorities[location];
+        return count;
     }
-
 }
