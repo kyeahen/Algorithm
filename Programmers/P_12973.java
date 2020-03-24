d5a5f91c7cdbe0d6fcc39a7cf1cf13e463ffd294
kyeahen <bright19961@naver.com>
2020-03-24 14:32:51 +0900

    Add 짝지어 제거하기 - level2

--- Programmers/P_12973.java
+++ Programmers/P_12973.java
@@ -1,4 +1,30 @@
 package Programmers;
+import java.util.Stack;
 
+//짝지어 제거하기 - level2
 public class P_12973 {
+
+    public static void main(String[] args) {
+        String s = "baabaa";
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return 1;
+        }
+
+        return 0;
+    }
 }
