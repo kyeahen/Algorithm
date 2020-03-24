e445cbf71d2c5ab52a1dab5eb26da241e6bac574
kyeahen <bright19961@naver.com>
2020-03-24 16:36:29 +0900

    Modify 짝지어 제거하기 - level2

--- Programmers/P_12973.java
+++ Programmers/P_12973.java
@@ -14,14 +14,15 @@ public class P_12973 {
 
         for (int i = 0; i < s.length(); i++) {
 
+            //스택이 차있는 상태에서 가장 위에 있는 데이터가 넣을 데이터와 같다면
             if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
-                stack.pop();
-            } else {
-                stack.push(s.charAt(i));
+                stack.pop(); //데이터 삭제
+            } else { //스택이 비어있다면
+                stack.push(s.charAt(i)); //데이터 삽입
             }
         }
 
-        if (stack.isEmpty()) {
+        if (stack.isEmpty()) { //스택이 비어있다면 문자열 모두 짝지어 제거한 상태
             return 1;
         }
 
