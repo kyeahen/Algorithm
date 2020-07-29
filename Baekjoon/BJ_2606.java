fb4830b489a1596332e699c6ebe33cd637dd9ca2
kyeahen <bright19961@naver.com>
2020-07-29 16:10:54 +0900

    Modify 2667, 2606, 42840 - 주석 추가 및 수정

--- Baekjoon/BJ_2606.java
+++ Baekjoon/BJ_2606.java
@@ -12,31 +12,33 @@ public class BJ_2606 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        computers = s.nextInt();
-        link = s.nextInt();
+        computers = s.nextInt(); //컴퓨터의 수
+        link = s.nextInt(); //네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
 
-        map = new int[computers + 1][computers + 1];
-        visted = new int[computers + 1];
+        map = new int[computers + 1][computers + 1]; // 컴퓨터 간의 연결을 나타내기 위한 배열
+        visted = new int[computers + 1]; // 컴퓨터 방문 여부 체크 배열
 
         for (int i = 0; i < link; i++) {
             int a = s.nextInt();
             int b = s.nextInt();
 
+            //서로 연결되어 있기에 1을 삽입
             map[a][b] = 1;
             map[b][a] = 1;
         }
 
-        dfs(1);
-        System.out.print(count);
+        dfs(1); //dfs 탐색
+        System.out.print(count); //바이러스에 걸리게 되는 컴퓨터 수 출력
     }
 
     public static void dfs(int start) {
-        visted[start] = 1;
+        visted[start] = 1; //방문한 컴퓨터 -> 1
 
         for (int i = 1; i <= computers; i++) {
-            if (map[start][i] == 1 && visted[i] == 0) {
-                dfs(i);
-                count++;
+
+            if (map[start][i] == 1 && visted[i] == 0) { //연결된 컴퓨터이면서 방문하지 않은 컴퓨터
+                dfs(i); //dfs 탐색
+                count++; //바이러스에 걸리게 되는 컴퓨터 증가
             }
         }
     }
--- Programmers/P_42840.java
+++ Programmers/P_42840.java
@@ -17,37 +17,38 @@ public class P_42840 {
         int[] ans2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] ans3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int[] count = new int[3];
+        int[] count = new int[3]; //수포자들이 맞춘 정답의 개수를 저장하는 배열
 
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == ans1[i % 5]) {
+
+            if (answers[i] == ans1[i % 5]) { //수포자1
                 count[0]++;
             }
 
-            if (answers[i] == ans2[i % 8]) {
+            if (answers[i] == ans2[i % 8]) { //수포자2
                 count[1]++;
             }
 
-            if (answers[i] == ans3[i % 10]) {
+            if (answers[i] == ans3[i % 10]) { //수포자3
                 count[2]++;
             }
         }
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>(); // 가장 많은 문제를 맞춘 사람을 저장하는 배열
 
         int max = -1;
-        for (int i = 0; i < count.length; i++) {
+        for (int i = 0; i < count.length; i++) { //최댓값 찾기
             if (max < count[i]) {
                max = count[i];
             }
         }
 
         for (int i = 0; i < count.length; i++) {
-            if (count[i] == max) {
+            if (count[i] == max) { // 가장 높은 점수를 받은 사람들 배열에 추가하기
                 arr.add(i + 1);
             }
         }
 
-        return arr.stream().mapToInt(i -> i).toArray();
+        return arr.stream().mapToInt(i -> i).toArray(); //ArrayList -> int[]로 변환하여 반환
     }
 }
