34e4dd84fd4c25da977dfdc409c009143c1ee82c
kyeahen <bright19961@naver.com>
2020-09-29 18:45:23 +0900

    Add 1476 - 완전탐색

---
+++ Baekjoon/BJ_1476.java
@@ -0,0 +1,40 @@
+package Baekjoon;
+import java.util.Scanner;
+
+//날짜계산 - 완전탐색
+public class BJ_1476 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int E = sc.nextInt();
+        int S = sc.nextInt();
+        int M = sc.nextInt();
+
+        int year = 1;
+        int e = 1, s = 1, m = 1;
+        while (true) {
+            if (e == E && s == S && m == M) {
+                break;
+            }
+
+            e++;
+            s++;
+            m++;
+
+            if (e > 15) {
+                e = 1;
+            }
+            if (s > 28) {
+                s = 1;
+            }
+            if (m > 19) {
+                m = 1;
+            }
+
+            year++;
+        }
+
+        System.out.println(year);
+    }
+}
